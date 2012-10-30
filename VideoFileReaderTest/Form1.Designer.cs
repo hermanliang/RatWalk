0dec4ff6343879782b586e8d73b4587d26d3b3df
Herman Liang <hermanliang80@gmail.com>
2012-10-30 21:40:08 +0800

    init commit
    Signed-off-by: Herman Liang <hermanliang80@gmail.com>

---
+++ VideoFileReaderTest/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace VideoFileReaderTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(569, 333);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(13, 381);
+            this.trackBar1.Maximum = 128;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(568, 42);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar2.Location = new System.Drawing.Point(13, 430);
+            this.trackBar2.Maximum = 500;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(568, 42);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 521);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ VideoFileReaderTest/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace VideoFileReaderTest
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture;
+        private Timer mTimer;
+        private int para1 = 3;
+        private double para2 = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                _capture = new Capture(ofd.FileName);
+                //pictureBox1.Image = _capture.QueryFrame().ToBitmap();
+                mTimer = new Timer();
+                mTimer.Interval = 1000 / 90;
+                mTimer.Tick += timer_Tick;
+                mTimer.Start();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> frame = _capture.QueryFrame();
+            if (frame != null)
+            {
+                Image<Gray, byte> gImage = frame.Convert<Gray, byte>();
+                gImage = gImage.ConvertScale<byte>(0.25, 0);
+                //Image<Gray, byte> gImage2 = gImage.Clone();
+                //gImage2 = gImage2.SmoothBlur((int)para2, (int)para2);
+                //gImage2 = gImage2.Not();
+                //gImage = gImage.AddWeighted(gImage2, 1, 0.5, 0);
+                //gImage._EqualizeHist();
+                gImage = gImage.ThresholdBinary(new Gray(para1), new Gray(255));
+                
+                //gImage = gImage.ThresholdAdaptive(
+                //    new Gray(255),
+                //    Emgu.CV.CvEnum.ADAPTIVE_THRESHOLD_TYPE.CV_ADAPTIVE_THRESH_MEAN_C,
+                //    Emgu.CV.CvEnum.THRESH.CV_THRESH_BINARY,
+                //    para1,
+                //    new Gray(para2)
+                //);
+                gImage = gImage.MorphologyEx(
+                    new StructuringElementEx(
+                        5,
+                        (int)para2,
+                        2,
+                        (int)para2 / 2,
+                        Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_ELLIPSE),
+                    Emgu.CV.CvEnum.CV_MORPH_OP.CV_MOP_OPEN,
+                    1);
+
+                pictureBox1.Image = gImage.ToBitmap();
+                //pictureBox1.Image = frame.ToBitmap();
+            }
+            else
+            {
+                mTimer.Stop();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            para1 = ((TrackBar)sender).Value * 2 + 1;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            para2 = (double)((TrackBar)sender).Value;
+            para2 = (int)para2 % 2 == 0 ? para2 + 1 : para2;
+        }
+    }
+}
