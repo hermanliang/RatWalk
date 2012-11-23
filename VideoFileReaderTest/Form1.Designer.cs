97dc8665f57431f7a522b10ccba7ef3262afb099
Herman Liang <hermanliang80@gmail.com>
2012-11-23 22:14:03 +0800

    add icon, modified private methods to static
    Signed-off-by: Herman Liang <hermanliang80@gmail.com>

--- VideoFileReaderTest/Form1.Designer.cs
+++ VideoFileReaderTest/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
@@ -37,12 +38,14 @@
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -53,15 +56,14 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Load";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.onLoadFile);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(163, 12);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.Size = new System.Drawing.Size(325, 243);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -77,7 +79,7 @@
             this.trackBar1.Size = new System.Drawing.Size(661, 45);
             this.trackBar1.TabIndex = 2;
             this.trackBar1.Value = 57;
-            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.Scroll += new System.EventHandler(this.onParam1_Scroll);
             // 
             // trackBar2
             // 
@@ -90,81 +92,101 @@
             this.trackBar2.Size = new System.Drawing.Size(661, 45);
             this.trackBar2.TabIndex = 3;
             this.trackBar2.Value = 17;
-            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackBar2.Scroll += new System.EventHandler(this.onParam2_Scroll);
             // 
             // trackBar3
             // 
             this.trackBar3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBar3.Location = new System.Drawing.Point(173, 516);
+            this.trackBar3.Location = new System.Drawing.Point(173, 546);
             this.trackBar3.Name = "trackBar3";
             this.trackBar3.Size = new System.Drawing.Size(501, 45);
             this.trackBar3.TabIndex = 4;
-            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            this.trackBar3.Scroll += new System.EventHandler(this.onSeekbar_Scroll);
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button2.Location = new System.Drawing.Point(13, 517);
+            this.button2.Location = new System.Drawing.Point(13, 547);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(74, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "Play";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.onPlay_Click);
             // 
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button3.Location = new System.Drawing.Point(93, 517);
+            this.button3.Location = new System.Drawing.Point(93, 547);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(74, 23);
             this.button3.TabIndex = 6;
             this.button3.Text = "Stop";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.onStop_Click);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(353, 258);
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(334, 252);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.Size = new System.Drawing.Size(325, 244);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(12, 258);
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 252);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox3.Size = new System.Drawing.Size(325, 244);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 8;
             this.pictureBox3.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 41);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(662, 499);
+            this.tableLayoutPanel1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(686, 686);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.trackBar3);
-            this.Controls.Add(this.trackBar2);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
-            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,6 +203,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- VideoFileReaderTest/Form1.cs
+++ VideoFileReaderTest/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using FTimer = System.Windows.Forms.Timer;
+using System.Threading;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.Util;
@@ -16,9 +18,11 @@ namespace VideoFileReaderTest
     public partial class Form1 : Form
     {
         private Capture _capture;
-        private Timer mTimer;
+        private FTimer mTimer;
         private int para1 = 57;
         private double para2 = 17;
+        private List<Rectangle> ratRect = new List<Rectangle>();
+        private List<Rectangle> redCircles = new List<Rectangle>();
 
         private double
             totalFrames,
@@ -30,34 +34,122 @@ namespace VideoFileReaderTest
         public Form1()
         {
             InitializeComponent();
+            tableLayoutPanel1.SetColumnSpan(pictureBox1, 2);
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                _capture = new Capture(ofd.FileName);
+        #region Private Static Methods
 
-                getBackgroundImage();
-
-                totalFrames = _capture.GetCaptureProperty(CAP_PROP.CV_CAP_PROP_FRAME_COUNT);
-                FPS = _capture.GetCaptureProperty(CAP_PROP.CV_CAP_PROP_FPS);
-                currentFrame = _capture.GetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES);
-                Console.WriteLine("fn: " + currentFrame);
-                trackBar3.Maximum = (int)totalFrames - 1;
-                trackBar3.Minimum = 0;
-
-            }
-        }
-
-        private void getBackgroundImage()
+        private static Image<Gray, byte> getBackgroundImage(
+            Capture _capture,
+            int para1,
+            int para2)
         {
             Image<Bgr, byte> frame = _capture.QueryFrame();
-            pictureBox1.Image = frame.ToBitmap();
-
-            bgImage = preProcessImage(frame);
+            //pictureBox1.Image = frame.ToBitmap();
+            List<Rectangle> ratRect;
+            return preProcessImage(frame,para1,para2, out ratRect);
         }
+
+        private static Image<Gray, byte> getRatContour(
+            Image<Bgr, byte> frame,
+            Image<Gray, byte> bgImage,
+            int para1,
+            int para2,
+            out List<Rectangle> ratRect)
+        {
+            Image<Gray, byte> gImage = preProcessImage(frame, para1, para2, out ratRect);
+            gImage = gImage.Sub(bgImage);
+            return gImage;
+        }
+
+        private static Image<Gray, byte> getRedCircle(
+            Image<Bgr, byte> frame,
+            out List<Rectangle> redCircles)
+        {
+            redCircles = new List<Rectangle>();
+            Image<Hsv, byte> hsvImage = frame.Convert<Hsv, byte>();
+            Image<Gray, byte>[] channels = hsvImage.Split();
+            Image<Gray, byte> HueImage = channels[0].InRange(new Gray(170), new Gray(180));
+            Image<Gray, byte> SatImage = channels[1].InRange(new Gray(70), new Gray(120));
+            Image<Gray, byte> ValueImage = channels[2].InRange(new Gray(200), new Gray(255));
+            Image<Gray, byte> cImage = HueImage.And(ValueImage);
+            cImage = cImage.And(SatImage);
+
+            cImage = cImage.Dilate(4);
+            cImage = cImage.Erode(5);
+
+            Contour<Point> contours = cImage.FindContours();
+
+            redCircles = findRedPosition(contours);
+            
+            return cImage;
+        }
+
+        private static List<Rectangle> findRedPosition(Contour<Point> contours)
+        {
+            List<Rectangle> redCircles = new List<Rectangle>();
+            for (; contours != null; contours = contours.HNext)
+            {
+                redCircles.Add(contours.BoundingRectangle);
+            }
+            return redCircles;
+        }
+
+        private static Image<Gray, byte> preProcessImage(
+            Image<Bgr, byte> frame,
+            int para1,
+            int para2,
+            out List<Rectangle> ratRect)
+        {
+            ratRect = new List<Rectangle>();
+            Image<Gray, byte> gImage = frame.Convert<Gray, byte>();
+            gImage = gImage.ConvertScale<byte>(0.25, 0);
+            //Image<Gray, byte> gImage2 = gImage.Clone();
+            //gImage2 = gImage2.SmoothBlur((int)para2, (int)para2);
+            //gImage2 = gImage2.Not();
+            //gImage = gImage.AddWeighted(gImage2, 1, 0.5, 0);
+            //gImage._EqualizeHist();
+            gImage = gImage.ThresholdBinary(new Gray(para1), new Gray(255));
+
+            //gImage = gImage.ThresholdAdaptive(
+            //    new Gray(255),
+            //    Emgu.CV.CvEnum.ADAPTIVE_THRESHOLD_TYPE.CV_ADAPTIVE_THRESH_MEAN_C,
+            //    Emgu.CV.CvEnum.THRESH.CV_THRESH_BINARY,
+            //    para1,
+            //    new Gray(para2)
+            //);
+            gImage = gImage.MorphologyEx(
+                new StructuringElementEx(
+                    3,
+                    (int)para2,
+                    1,
+                    (int)para2 / 2,
+                    Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_ELLIPSE),
+                Emgu.CV.CvEnum.CV_MORPH_OP.CV_MOP_OPEN,
+                1);
+
+            Contour<Point> contours = gImage.FindContours();
+            ratRect = findRatContour(contours);
+
+            return gImage;
+        }
+
+        private static List<Rectangle> findRatContour(Contour<Point> contours)
+        {
+            List<Rectangle> ratRect = new List<Rectangle>();
+            for (; contours != null; contours = contours.HNext)
+            {
+                if (contours.Area > 10000)
+                {
+                    ratRect.Add(contours.BoundingRectangle);
+                }
+            }
+            return ratRect;
+        }
+
+        #endregion
+
+        #region Thread(Timer) Handler
 
         private void timer_Tick(object sender, EventArgs e)
         {
@@ -70,10 +162,15 @@ namespace VideoFileReaderTest
                 trackBar3.Value = (int)currentFrame;
                 Console.WriteLine("fn: " + currentFrame);
 
-                Image<Gray, byte> gImage = getRatContour(frame);
+                Image<Gray, byte> gImage = getRatContour(
+                    frame,
+                    bgImage,
+                    para1,
+                    (int)para2,
+                    out ratRect);
                 pictureBox2.Image = gImage.ToBitmap();
 
-                Image<Gray, byte> cImage = getRedCircle(frame);
+                Image<Gray, byte> cImage = getRedCircle(frame, out redCircles);
                 pictureBox3.Image = cImage.ToBitmap();
 
                 foreach (Rectangle rect in redCircles)
@@ -104,148 +201,104 @@ namespace VideoFileReaderTest
             }
         }
 
-        private Image<Gray, byte> getRatContour(Image<Bgr, byte> frame)
+        #endregion
+
+        #region Event Handler
+
+        private void onLoadFile(object sender, EventArgs e)
         {
-            Image<Gray, byte> gImage = preProcessImage(frame);
-            gImage = gImage.Sub(bgImage);
-            return gImage;
-        }
-
-        private List<Rectangle> redCircles = new List<Rectangle>();
-
-        private Image<Gray, byte> getRedCircle(Image<Bgr, byte> frame)
-        {
-            Image<Hsv, byte> hsvImage = frame.Convert<Hsv, byte>();
-            Image<Gray, byte>[] channels = hsvImage.Split();
-            Image<Gray, byte> HueImage = channels[0].InRange(new Gray(170), new Gray(180));
-            Image<Gray, byte> SatImage = channels[1].InRange(new Gray(70), new Gray(120));
-            Image<Gray, byte> ValueImage = channels[2].InRange(new Gray(200), new Gray(255));
-            Image<Gray, byte> cImage = HueImage.And(ValueImage);
-            cImage = cImage.And(SatImage);
-
-            cImage = cImage.Dilate(4);
-            cImage = cImage.Erode(5);
-
-            Contour<Point> contours = cImage.FindContours();
-
-            findRedPosition(contours);
-            
-            
-            return cImage;
-        }
-
-        private void findRedPosition(Contour<Point> contours)
-        {
-            redCircles.Clear();
-            for (; contours != null; contours = contours.HNext)
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
-                redCircles.Add(contours.BoundingRectangle);
+                _capture = new Capture(ofd.FileName);
+
+                bgImage = getBackgroundImage(
+                    _capture,
+                    para1,
+                    (int)para2);
+
+                totalFrames = _capture.GetCaptureProperty(CAP_PROP.CV_CAP_PROP_FRAME_COUNT);
+                FPS = _capture.GetCaptureProperty(CAP_PROP.CV_CAP_PROP_FPS);
+                currentFrame = _capture.GetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES);
+                Console.WriteLine("fn: " + currentFrame);
+                trackBar3.Maximum = (int)totalFrames - 1;
+                trackBar3.Minimum = 0;
+
             }
         }
 
-        private Image<Gray, byte> preProcessImage(Image<Bgr, byte> frame)
+        private void onParam1_Scroll(object sender, EventArgs e)
         {
-            Image<Gray, byte> gImage = frame.Convert<Gray, byte>();
-            gImage = gImage.ConvertScale<byte>(0.25, 0);
-            //Image<Gray, byte> gImage2 = gImage.Clone();
-            //gImage2 = gImage2.SmoothBlur((int)para2, (int)para2);
-            //gImage2 = gImage2.Not();
-            //gImage = gImage.AddWeighted(gImage2, 1, 0.5, 0);
-            //gImage._EqualizeHist();
-            gImage = gImage.ThresholdBinary(new Gray(para1), new Gray(255));
+            try
+            {
+                (new Thread(delegate() { changeBackgroundImage(sender); })).Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
-            //gImage = gImage.ThresholdAdaptive(
-            //    new Gray(255),
-            //    Emgu.CV.CvEnum.ADAPTIVE_THRESHOLD_TYPE.CV_ADAPTIVE_THRESH_MEAN_C,
-            //    Emgu.CV.CvEnum.THRESH.CV_THRESH_BINARY,
-            //    para1,
-            //    new Gray(para2)
-            //);
-            gImage = gImage.MorphologyEx(
-                new StructuringElementEx(
-                    3,
-                    (int)para2,
-                    1,
-                    (int)para2 / 2,
-                    Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_ELLIPSE),
-                Emgu.CV.CvEnum.CV_MORPH_OP.CV_MOP_OPEN,
-                1);
-
-            Contour<Point> contours = gImage.FindContours();
-            findRatContour(contours);
-
-            return gImage;
         }
 
-        List<Rectangle> ratRect = new List<Rectangle>();
-
-        private void findRatContour(Contour<Point> contours)
+        private delegate void changeBackgroundImageCallback(object sender);
+        private void changeBackgroundImage(object sender)
         {
-            ratRect.Clear();
-            for (; contours != null; contours = contours.HNext)
+            if (this.InvokeRequired)
             {
-                if (contours.Area > 10000)
+                changeBackgroundImageCallback cbic = new changeBackgroundImageCallback(changeBackgroundImage);
+                this.Invoke(cbic, sender);
+            }
+            else
+            {
+                bool isAlive = false;
+                if (mTimer != null && mTimer.Enabled)
                 {
-                    ratRect.Add(contours.BoundingRectangle);
+                    isAlive = true;
+                    mTimer.Stop();
+                }
+                para1 = ((TrackBar)sender).Value;
+                _capture.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES, 0);
+                bgImage = getBackgroundImage(
+                    _capture,
+                    para1,
+                    (int)para2);
+                _capture.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES, currentFrame);
+                if (isAlive)
+                {
+                    mTimer.Start();
                 }
             }
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            bool isAlive = false;
-            if (mTimer != null && mTimer.Enabled)
-            {
-                isAlive = true;
-                mTimer.Stop();
-            }
-            para1 = ((TrackBar)sender).Value;
-            _capture.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES, 0);
-            getBackgroundImage();
-            _capture.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES, currentFrame);
-            if (isAlive)
-            {
-                mTimer.Start();
-            }
-        }
-
-        private void trackBar2_Scroll(object sender, EventArgs e)
+        private void onParam2_Scroll(object sender, EventArgs e)
         {
             para2 = (double)((TrackBar)sender).Value;
             para2 = (int)para2 % 2 == 0 ? para2 + 1 : para2;
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            _capture.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES, currentFrame);
-            mTimer = new Timer();
-
-            mTimer.Interval = (int)(1000 / FPS);
-            mTimer.Tick += timer_Tick;
-            mTimer.Start();
-        }
-
-        private void trackBar3_Scroll(object sender, EventArgs e)
+        private void onSeekbar_Scroll(object sender, EventArgs e)
         {
             currentFrame = (double)trackBar3.Value;
             _capture.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES, currentFrame);
             pictureBox1.Image = _capture.QueryFrame().ToBitmap();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void onPlay_Click(object sender, EventArgs e)
+        {
+            _capture.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_POS_FRAMES, currentFrame);
+            mTimer = new FTimer();
+
+            mTimer.Interval = (int)(1000 / FPS);
+            mTimer.Tick += timer_Tick;
+            mTimer.Start();
+        }
+
+        private void onStop_Click(object sender, EventArgs e)
         {
             mTimer.Stop();
         }
 
-        private void Form1_SizeChanged(object sender, EventArgs e)
-        {
-            int winWidth = this.Width;
-            int picBoxWidth = (winWidth - 30) / 2;
-            pictureBox2.Width = picBoxWidth;
-            pictureBox3.Width = picBoxWidth;
-            pictureBox3.Left = 12;
-            pictureBox2.Left = 17 + picBoxWidth;
+        #endregion
 
-        }
     }
 }
